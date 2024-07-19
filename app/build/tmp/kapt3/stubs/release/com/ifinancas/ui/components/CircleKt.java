3cca62269d279e043428b3a2be0a4479a79c7d5b
eliezer <eliezerassuncaocustodio@hotmail.com>
2024-07-19 15:12:36 -0300

    ajustes

--- app/build/tmp/kapt3/stubs/release/com/ifinancas/ui/components/CircleKt.java
+++
@@ -1,9 +0,0 @@
-package com.ifinancas.ui.components;
-
-import androidx.compose.runtime.Composable;
-import androidx.compose.ui.Modifier;
-import androidx.compose.ui.unit.Dp;
-
-@kotlin.Metadata(mv = {1, 9, 0}, k = 2, xi = 48, d1 = {"\u0000\u0016\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0003\u001a)\u0010\u0000\u001a\u00020\u00012\b\b\u0002\u0010\u0002\u001a\u00020\u00032\b\b\u0002\u0010\u0004\u001a\u00020\u0005H\u0007\u00f8\u0001\u0000\u00f8\u0001\u0001\u00a2\u0006\u0004\b\u0006\u0010\u0007\u0082\u0002\u000b\n\u0005\b\u00a1\u001e0\u0001\n\u0002\b\u0019\u00a8\u0006\b"}, d2 = {"Circle", "", "size", "Landroidx/compose/ui/unit/Dp;", "bgColor", "Landroidx/compose/ui/graphics/Color;", "Circle-cXLIe8U", "(FJ)V", "app_release"})
-public final class CircleKt {
-}
